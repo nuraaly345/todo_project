ceff550e221b020cbbb0ac936db365b7477c19f0
nuraaly345 <kubanura57@gmail.com>
2022-07-30 12:53:15 +0300

    TODO учун жазуу тузугоо модел кошу

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+admin.site.register(ToDo)
